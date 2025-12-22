429f4b948146985f2c6af9709fd4c137e94ef419
lokipr6190 <76768106+lokipr6190@users.noreply.github.com>
2025-12-22 23:12:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 
 // Database connection configuration
 const pool = new Pool({
-  host: process.env.DB_HOST || 'postgres-db-hits', // Service name in OpenShift
-  user: process.env.DB_USER || 'user',
-  password: process.env.DB_PASSWORD || 'pass',
-  database: process.env.DB_NAME || 'hitsdb',
+  host: process.env.POSTGRESQL_HOST || 'postgres-db-hits', // Service name in OpenShift
+  user: process.env.POSTGRESQL_USER || 'user2',
+  password: process.env.POSTGRESQL_PASSWORD || 'password123',
+  database: process.env.POSTGRESQL_DATABASE || 'hits_data',
   port: 5432,
 });
 
